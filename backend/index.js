c26aac51c8920fd78175dded355f75b80edd1d9b
m-dibakar <malakar.dibakar@icloud.com>
2026-02-17 19:22:33 +0530

    Connected MongoDB with express

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const express = require("express");
+const mongoose = require('mongoose');
+
+
+
+const PORT = process.env.PORT || 3002;
+const uri = process.env.MONGO_URL;
+
+main()
+ .then(()=>{
+    console.log("DB connected successfully...")
+ })
+ .catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(uri);
+}
+
+const app = express();
+
+app.listen(PORT,()=>{
+    console.log("App started...")
+})
